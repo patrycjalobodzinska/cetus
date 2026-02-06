ee35c4953b2a0a677e048ce851ebd98546479261
patrycjalobodzinska <patrycjalobodzinska.work@gmail.com>
2026-02-06 11:30:48 +0100

    fixy

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,7 +2,7 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2021-06-07'
 
 export const dataset =
-  process.env.NEXT_PUBLIC_SANITY_DATASET || 'development'
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export const projectId =
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'bjy1qdxm'
